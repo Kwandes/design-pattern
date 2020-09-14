e5fd92ca3476c42b56101448e09d5f48502a4e3b
Jan <jan.bogoryja@gmail.com>
2020-09-14 09:18:11 +0200

    Add encryption and decryption methods

--- src/main/java/dev/hotdeals/design_pattern/DesignPatternApplication.java
+++ src/main/java/dev/hotdeals/design_pattern/DesignPatternApplication.java
@@ -12,10 +12,19 @@ public class DesignPatternApplication
         System.out.println("Password: " + password);
         String encodedPassword = PasswordService.encodePassword(password);
         System.out.println("Encoded Password: " + encodedPassword);
+
+
         encodedPassword = PasswordService.encodePassword(password);
         System.out.println("Encoded Password: " + encodedPassword);
         encodedPassword = PasswordService.encodePassword(password);
         System.out.println("Encoded Password: " + encodedPassword);
+
+        System.out.println("\n\n");
+        String text = "very high value data";
+        System.out.println("Text to encrypt: " + text);
+        String encryptedText = PasswordService.encrypt(text);
+        System.out.println("Encrypted text: " + encryptedText);
+        System.out.println("Decrypted text: " + PasswordService.decrypt(encryptedText));
     }
 
 }
--- src/main/java/dev/hotdeals/design_pattern/PasswordService.java
+++ src/main/java/dev/hotdeals/design_pattern/PasswordService.java
@@ -1,10 +1,14 @@
 package dev.hotdeals.design_pattern;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
 
 public class PasswordService
 {
-    private static final int encodingStrength = 12; // work factor of bcrypt
+    private static final int ENCODING_STRENGTH = 12; // work factor of bcrypt
+    private static final String ENCRYPTION_KEY = "very secure key";
+    private static final String ENCRYPTION_SALT = "9e965ac7dbbcc628";
 
     /**
      * Produces a hashed and salted version of a string using Springboots' Bcrypt encoder.
@@ -15,7 +19,7 @@ public class PasswordService
      */
     public static String encodePassword(String password)
     {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(encodingStrength);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(ENCODING_STRENGTH);
         return encoder.encode(password);
     }
 
@@ -28,7 +32,19 @@ public class PasswordService
      */
     public static boolean matches(String password, String password2)
     {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(encodingStrength);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(ENCODING_STRENGTH);
         return encoder.matches(password, password2);
+    }
+
+    public static String encrypt(String text)
+    {
+        TextEncryptor encryptor = Encryptors.text(ENCRYPTION_KEY, ENCRYPTION_SALT);
+        return encryptor.encrypt(text);
+    }
+
+    public static String decrypt(String text)
+    {
+        TextEncryptor encryptor = Encryptors.text(ENCRYPTION_KEY, ENCRYPTION_SALT);
+        return encryptor.decrypt(text);
     }
 }
